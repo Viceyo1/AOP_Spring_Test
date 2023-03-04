9894df46f8b4d4dace2b5e216b4f77f2dcfabe4a
ViceWong <2803107606@qq.com>
2023-03-04 15:02:05 +0800

    APO_Example_BaiduWangPan(Answer)

---
+++ src/main/java/com/itheima/aop/DataAdvice.java
@@ -0,0 +1,33 @@
+package com.itheima.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class DataAdvice {
+    @Pointcut("execution(* com.itheima.service.ResourcesService.openURL(*,*))")
+    private void servicePt(){}
+
+    @Around("servicePt()")
+    public Object trimStr(ProceedingJoinPoint pjp) throws Throwable {
+        Object[] args = pjp.getArgs();
+
+        for (int i = 0; i < args.length; i++) {
+            //判断参数是否是字符串
+            if (args[i].getClass().equals(String.class)){
+                args[i] = args[i].toString().trim();
+            }
+        }
+        Object ret = pjp.proceed(args);
+        return ret;
+
+    }
+
+
+
+
+}
